0a0c764909822ad24ad8c5d33b88d85e3b65ca56
Marina Adams <bardakover@gmail.com>
2025-05-05 21:56:38 -0700

    add solutions for part1: factorial, reverse, bunny, is_nested_parens; solutions for part2: is palindrome, digit_match

--- part-1.py
+++ part-1.py
@@ -4,17 +4,31 @@
 # the appropriate comment.
 
 # factorial
-
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
 
 # reverse
-
-
+def reverse(text):
+    return text[::-1]
 
 # bunny
-
-
+def bunny(count):
+    if count == 0:
+        return 0
+    else:
+        return 2 + bunny(count - 1)
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
+    elif parens[:1] != "(" or parens[-1:] != ")":
+        return False
+    else:
+        return is_nested_parens(parens[1:-1])
 
 
--- part-2.py
+++ part-2.py
@@ -4,13 +4,31 @@
 # the appropriate comment.
 
 # search
-
-
+def search(array, query):
+    if not array:
+        return False
+    if array[0] == query:
+        return True
+    return search(array[1:], query)
 
 # is_palindrome
-
-
+def is_palindrome(text):
+    if text == "":
+        return True
+    if text[0] != text[-1]:
+        return False
+    return is_palindrome(text[1:-1])
 
 # digit_match
+def digit_match(a, b):
+    def helper(x, y):
+        if x == 0 or y == 0:
+            return 0  
 
+        last_match = 1 if x % 10 == y % 10 else 0
+        return last_match + helper(x // 10, y // 10)
+
+    if a == 0 and b == 0:
+        return 1
+    return helper(a, b)
 
